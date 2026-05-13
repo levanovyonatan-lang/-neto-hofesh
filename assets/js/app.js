1ed554bf10f8fb5ac25773bec17c046226550d4a
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-05-13 21:02:00 +0300

    Implement 100% Edge-to-Edge banner for all devices (v116)

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,7 +45,7 @@ const allTargets = [
 
 function initPWA() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js?v=114').catch(() => { });
+        navigator.serviceWorker.register('sw.js?v=116').catch(() => { });
     }
 
     const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone) || window.matchMedia('(display-mode: standalone)').matches;
