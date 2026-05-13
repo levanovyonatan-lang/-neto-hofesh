25f28502e32235142c5e60dd2905930217d08097
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-05-13 19:13:44 +0300

    Bump version to 103 for cache refresh

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,7 +45,7 @@ const allTargets = [
 
 function initPWA() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js?v=102').catch(() => { });
+        navigator.serviceWorker.register('sw.js?v=103').catch(() => { });
     }
 
     const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone) || window.matchMedia('(display-mode: standalone)').matches;
