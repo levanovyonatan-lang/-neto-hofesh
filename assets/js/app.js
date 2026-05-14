603ba16d76395c7a7e068c7dcfe536266e992954
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-05-14 11:04:23 +0300

    Final 320x60 mobile banner update (v140)

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,7 +45,7 @@ const allTargets = [
 
 function initPWA() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js?v=137').catch(() => { });
+        navigator.serviceWorker.register('sw.js?v=140').catch(() => { });
     }
 
     const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone) || window.matchMedia('(display-mode: standalone)').matches;
