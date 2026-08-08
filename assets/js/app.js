527683363f79d6435fd0d0a74ba3ab014f8cb00d
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-08-09 01:25:21 +0300

    Update community text to 'קהילה של נטו חופש'

--- assets/js/app.js
+++ assets/js/app.js
@@ -87,7 +87,7 @@ function initPWA() {
         const hadController = !!navigator.serviceWorker.controller;
         let isRefreshing = false;
 
-        navigator.serviceWorker.register('sw.js?v=310').then(reg => {
+        navigator.serviceWorker.register('sw.js?v=311').then(reg => {
             reg.update();
 
             setInterval(() => {
@@ -1162,7 +1162,7 @@ function showMainScreen() {
     if (!document.getElementById('dino-school-script') && typeof window.startDinoGame !== 'function') {
         const script = document.createElement('script');
         script.id = 'dino-school-script';
-        script.src = 'assets/js/games/dino-school.js?v=310';
+        script.src = 'assets/js/games/dino-school.js?v=311';
         document.body.appendChild(script);
     }
 
