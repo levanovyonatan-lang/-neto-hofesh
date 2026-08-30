8a6e6c11b7e3696a9532290952b2b43e538d86c4
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-08-30 16:53:22 +0300

    Revert "Implement 365 daily tips and missions from PDF"
    This reverts commit e81e03264da78200f7d0b7c2c52b0a4d8696ad84.

--- check_pdf_reverse.py
+++
@@ -1,6 +0,0 @@
-with open(r"C:\Users\user\נטו חופש\neto-hofesh\test_pymupdf.txt", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-with open(r"C:\Users\user\נטו חופש\neto-hofesh\test_reversed.txt", "w", encoding="utf-8") as f:
-    for line in lines[:20]:
-        f.write(line.strip()[::-1] + "\n")
