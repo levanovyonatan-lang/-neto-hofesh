b3df8d45726127aed085631faa4e8c4df5444320
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-08 18:08:00 +0300

    Apply strong yellow border only to gold theme

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4286';
-const CACHE_NAME = 'neto-cache-v4286';
+const CACHE_VERSION = 'v4287';
+const CACHE_NAME = 'neto-cache-v4287';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
