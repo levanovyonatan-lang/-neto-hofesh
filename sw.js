416f3f32a4d2916ec325e0308c6bbce5a3203346
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-08 23:49:36 +0300

    Fix input width overflow on mobile in custom countdown modal

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4294';
-const CACHE_NAME = 'neto-cache-v4294';
+const CACHE_VERSION = 'v4295';
+const CACHE_NAME = 'neto-cache-v4295';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
