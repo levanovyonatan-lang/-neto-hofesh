41de8fc5558b27308c2ca6a7187c39986bbafd91
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-08 23:57:49 +0300

    Make custom countdown modal background solid

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4298';
-const CACHE_NAME = 'neto-cache-v4298';
+const CACHE_VERSION = 'v4299';
+const CACHE_NAME = 'neto-cache-v4299';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
