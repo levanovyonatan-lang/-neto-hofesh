60e8f9bf7ee6baab88971efb6c314b9884ecf05c
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-08 23:54:18 +0300

    Fix input width overflow on mobile: remove width 100% from wrappers and add webkit-appearance none

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4296';
-const CACHE_NAME = 'neto-cache-v4296';
+const CACHE_VERSION = 'v4297';
+const CACHE_NAME = 'neto-cache-v4297';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
