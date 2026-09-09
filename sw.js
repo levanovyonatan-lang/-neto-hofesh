4fb8d3c5cf18563f1e8cd13c73d0dcc378783875
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-09 20:44:48 +0300

    style: restore premium golden style for custom countdown btn when in VIP mode

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4303';
-const CACHE_NAME = 'neto-cache-v4303';
+const CACHE_VERSION = 'v4304';
+const CACHE_NAME = 'neto-cache-v4304';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
