f735bf06a278f320f6fe5bbd70b1651705f0b9bd
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-09 20:47:36 +0300

    style: revert standard custom countdown btn to original translucent design

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4304';
-const CACHE_NAME = 'neto-cache-v4304';
+const CACHE_VERSION = 'v4305';
+const CACHE_NAME = 'neto-cache-v4305';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
