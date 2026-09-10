c1ed750fe7b1e907b4028f77d08cfb35a65b3862
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-10 15:58:37 +0300

    fix: remove inline max-height on custom emoji grid, bump cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4312';
-const CACHE_NAME = 'neto-cache-v4312';
+const CACHE_VERSION = 'v4313';
+const CACHE_NAME = 'neto-cache-v4313';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
