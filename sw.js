5826540ef70c559598fd80119e76bfad7db26fc6
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-11 23:55:05 +0300

    Restore detailed demo scenery with restrained visual effects

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿const CACHE_VERSION = 'v4320';
-const CACHE_NAME = 'neto-cache-v4320';
+﻿const CACHE_VERSION = 'v4321';
+const CACHE_NAME = 'neto-cache-v4321';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
