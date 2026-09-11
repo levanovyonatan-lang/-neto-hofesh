43df73b68080b97d564948996e93bd9b0c2d4dfc
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-11 13:00:04 +0300

    revert: rollback all changes to original state safely (app.js, sw.js, daily-tips.json) with bumped cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿const CACHE_VERSION = 'v4315';
-const CACHE_NAME = 'neto-cache-v4315';
+﻿const CACHE_VERSION = 'v4314';
+const CACHE_NAME = 'neto-cache-v4314';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
