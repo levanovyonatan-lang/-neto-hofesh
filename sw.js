47a2100d63d4749e7206e09f46fa6360be9ede5f
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-12 19:07:00 +0300

    Completely redesign custom personal countdown themes from scratch with premium glassmorphism

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4343';
-const CACHE_NAME = 'neto-cache-v4343';
+const CACHE_VERSION = 'v4344';
+const CACHE_NAME = 'neto-cache-v4344';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
