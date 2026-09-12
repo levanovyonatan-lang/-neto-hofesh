616cf26b399391d81f759359ddec5f273e612989
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-12 19:31:44 +0300

    Add premium 3D and parallax CSS themes for custom countdowns

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4346';
-const CACHE_NAME = 'neto-cache-v4346';
+const CACHE_VERSION = 'v4347';
+const CACHE_NAME = 'neto-cache-v4347';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
