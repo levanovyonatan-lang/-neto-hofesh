2353239826eaf00b13b998be447a9d30534236a2
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-12 15:58:49 +0300

    Implement subtle aesthetic card design overrides for themes to maintain readability

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4335';
-const CACHE_NAME = 'neto-cache-v4335';
+const CACHE_VERSION = 'v4337';
+const CACHE_NAME = 'neto-cache-v4337';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
