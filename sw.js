b04f0cffa9dd139f46935af4fd94b381aa2d16f9
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-13 14:52:58 +0300

    Remove Cookie Consent Banner as per user request

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4379';
-const CACHE_NAME = 'neto-cache-v4379';
+const CACHE_VERSION = 'v4380';
+const CACHE_NAME = 'neto-cache-v4380';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
