d111eed3498908807cdde550988a0e4b93f71fcf
Yonatan Levanov <levanov.yonatan@gmail.com>
2026-09-13 13:41:50 +0300

    Fix body flex-direction to stack footer properly

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'v4371';
-const CACHE_NAME = 'neto-cache-v4371';
+const CACHE_VERSION = 'v4372';
+const CACHE_NAME = 'neto-cache-v4372';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
